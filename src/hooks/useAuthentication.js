914ec664f2c1dd4fcbd7e7b585786defe918f985
josuerx12 <josuerx12@gmail.com>
2023-06-06 13:35:32 -0300

    updates.

--- src/hooks/useAuthentication.js
+++ src/hooks/useAuthentication.js
@@ -60,6 +60,36 @@ export const useAuthentication = () => {
     }
   };
 
+  //logout sign out
+
+  const logout = () => {
+    checkIfIsCancelled();
+    signOut(auth);
+  };
+
+  // login sign in
+  const login = async (data) => {
+    checkIfIsCancelled();
+    setLoading(true);
+    setError(false);
+
+    try {
+      await signInWithEmailAndPassword(auth, data.email, data.password);
+      setLoading(false);
+    } catch (error) {
+      let systemErrorMessage;
+      if (error.message.includes("user-not-found")) {
+        systemErrorMessage = "Usuário não encontrado.";
+      } else if (error.message.includes("wrong-password")) {
+        systemErrorMessage = "Senha Incorreta.";
+      } else {
+        systemErrorMessage = "Ocorreu um erro, por favor tente mais tarde.";
+      }
+      setError(systemErrorMessage);
+      setLoading(false);
+    }
+  };
+
   useEffect(() => {
     return () => setCancelled(true);
   }, []);
@@ -69,5 +99,7 @@ export const useAuthentication = () => {
     createUser,
     error,
     loading,
+    logout,
+    login,
   };
 };
