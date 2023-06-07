bb22545b3d285c74732af0284bb5a2ffc1c8f92d
josuerx12 <josuerx12@gmail.com>
2023-06-07 17:38:07 -0300

    updates

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,48 @@
 import styles from "./Home.module.css";
+
+//hooks
+import { useNavigate, Link } from "react-router-dom";
+import { useState } from "react";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+
+//components
+
 const Home = () => {
+  const [query, setQuery] = useState("");
+  const { documents: posts, loading } = useFetchDocuments("posts");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div>
-      <h1>Home</h1>
+    <div className={styles.home}>
+      <h1>Veja os nossos posts mais recentes</h1>
+      <form onSubmit={handleSubmit} className={styles.search_form}>
+        <input
+          type="text"
+          placeholder="Ou busque por tags..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="btn btn-dark">Pesquisar</button>
+      </form>
+      <div>
+        {loading && <p>Carregando...</p>}
+        {posts &&
+          posts.map((post) => (
+            <>
+              <h3>{post.title}</h3>
+            </>
+          ))}
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <p>Não foram encontrados posts</p>
+            <Link className="btn" to="/posts/create">
+              Criar primeiro post
+            </Link>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
